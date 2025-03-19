99ca79f1243d48eea410cc57972a169bd4a9d5fd
Matthew <madofuller@gmail.com>
2025-03-19 13:11:16 -0500

    another fix?

--- src/components/CTA.tsx
+++ src/components/CTA.tsx
@@ -4,28 +4,7 @@ import Script from "next/script";
 const CTA: React.FC = () => {
   return (
     <>
-      <iframe
-        src="https://api.leadconnectorhq.com/widget/form/vPkCsjuA7PYlyMi9XQlm"
-        style={{
-          width: "100%",
-          height: "100%",
-          border: "none",
-          borderRadius: "3px",
-        }}
-        id="inline-vPkCsjuA7PYlyMi9XQlm"
-        data-layout="{'id':'INLINE'}"
-        data-trigger-type="alwaysShow"
-        data-trigger-value=""
-        data-activation-type="alwaysActivated"
-        data-activation-value=""
-        data-deactivation-type="neverDeactivate"
-        data-deactivation-value=""
-        data-form-name="CRM Software Registration"
-        data-height="919"
-        data-layout-iframe-id="inline-vPkCsjuA7PYlyMi9XQlm"
-        data-form-id="vPkCsjuA7PYlyMi9XQlm"
-        title="CRM Software Registration"
-      ></iframe>
+
       <Script src="https://link.msgsndr.com/js/form_embed.js" strategy="afterInteractive" />
     </>
   );
